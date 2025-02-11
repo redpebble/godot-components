4449bf27bd3b732818377f1d952ebf286d1f4492
Jasper Andrew <9101110+jasperandrew@users.noreply.github.com>
2025-02-10 19:04:25 -0500

    minor cleanup

--- radial-menu/demo.gd
+++ radial-menu/demo.gd
@@ -4,12 +4,12 @@ var test_item_scene = preload("res://menu_item.tscn")
 
 func _ready() -> void:
 	var item_list: Array[MenuItem] = []
-	for i in $RadialMenu.num_slots - 5:
+	for i in $RadialMenu.num_slots:
 		var item = test_item_scene.instantiate() as MenuItem
 		item.get_icon().set_modulate(Color(randf(), randf(), randf()))
 		item_list.append(item)
 	$RadialMenu.set_item_list(item_list)
 	$RadialMenu.instantiate()
 
-func _on_radial_menu_item_selected(index: int, item: MenuItem) -> void:
+func _on_radial_menu_item_selected(index: int, _item: MenuItem) -> void:
 	print('item selected: %s'%[index])
--- radial-menu/radial_menu.gd
+++ radial-menu/radial_menu.gd
@@ -33,7 +33,7 @@ func instantiate():
 		
 		call_deferred('add_child', item)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var input_vector = Input.get_vector('ui_left', 'ui_right', 'ui_up', 'ui_down')
 	if input_vector == Vector2.ZERO:
 		$Cursor.hide()
