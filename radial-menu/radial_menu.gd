5849363d5841b434f5535971329a5fe41d4ed007
Jasper Andrew <9101110+jasperandrew@users.noreply.github.com>
2025-02-10 21:41:10 -0500

    finish static cursor logic

--- radial-menu/radial_menu.gd
+++ radial-menu/radial_menu.gd
@@ -20,8 +20,9 @@ var _item_list: Array[MenuItem] = []
 var _slot_index: int = -1
 
 # static cursor mode
+var _is_rotating = false
+var _current_rotation = 0
 var _menu_offset = 0
-var _rotating = false
 var _input_offset = 0
 
 func set_item_list(list: Array[MenuItem]):
@@ -31,24 +32,19 @@ func set_item_list(list: Array[MenuItem]):
 func instantiate():
 	$Cursor/Sprite.position.y = -(menu_radius - (200 * item_scale_ratio))
 	$Cursor/Sprite.scale = _item_scale
-	for i in num_slots:
-		if i >= _item_list.size(): return
-		var item = _item_list[i]
-		var content = item.get_content()
-		var angle = _angle_interval * i
-		item.rotation = angle
-		content.position.y = -menu_radius # so 0th item is "up"
-		content.rotation = -angle
-		content.scale = _item_scale
-		
+
+	if static_cursor:
+		$Cursor.rotation = 0
+
+	for item in _item_list:
 		call_deferred('add_child', item)
+	_update_items()
 
 func _process(_delta: float) -> void:
 	var input_vector = Input.get_vector('ui_left', 'ui_right', 'ui_up', 'ui_down')
 	if static_cursor: _highlight_with_static_cursor(input_vector)
 	else: _highlight_with_radial_cursor(input_vector)
-			
-	
+
 	if Input.is_action_just_pressed("ui_select"): _emit_item_selected()
 
 func _highlight_with_radial_cursor(input_vector: Vector2):
@@ -56,7 +52,7 @@ func _highlight_with_radial_cursor(input_vector: Vector2):
 			$Cursor.hide()
 			if !maintain_highlight: _un_highlight_all()
 			return
-		
+
 		var input_angle = _get_input_angle(input_vector)
 		$Cursor.show()
 		$Cursor.rotation = input_angle
@@ -64,21 +60,34 @@ func _highlight_with_radial_cursor(input_vector: Vector2):
 
 func _highlight_with_static_cursor(input_vector: Vector2):
 	if input_vector == Vector2.ZERO:
-		_menu_offset = rotation
-		_rotating = false
+		_current_rotation = _get_nearest_slot_angle(_current_rotation)
+		_update_items()
+		_menu_offset = _current_rotation
+		_is_rotating = false
 		return
-	
+
 	var input_angle = _get_input_angle(input_vector)
-	if !_rotating:
+	if !_is_rotating:
 		_input_offset = input_angle
-		_rotating = true
+		_is_rotating = true
 	input_angle -= _input_offset
 	input_angle += _menu_offset
-	rotation = input_angle
-	$Cursor.show()
-	$Cursor.rotation = -input_angle
-	
+
+	_current_rotation = input_angle
+	_update_items()
+
 	_highlight_nearest_slot(-input_angle)
+
+func _update_items():
+	for i in num_slots:
+		if i >= _item_list.size(): return
+		var item = _item_list[i]
+		var content = item.get_content()
+		var angle = _angle_interval * i + _current_rotation
+		item.rotation = angle
+		content.position.y = -menu_radius # so 0th item is "up"
+		content.rotation = -angle
+		content.scale = _item_scale
 
 func _highlight_nearest_slot(angle: float): # this could be made more efficient
 	angle = _normalize_angle(angle)
@@ -90,6 +99,17 @@ func _highlight_nearest_slot(angle: float): # this could be made more efficient
 			min_diff = diff
 			index = i
 	if _slot_index != index: _highlight_slot(index)
+
+func _get_nearest_slot_angle(angle: float) -> float: # this could be made more efficient
+	angle = _normalize_angle(angle)
+	var min_diff = PI
+	var nearest_angle = 0
+	for i in num_slots:
+		var diff = _angle_diff(angle, i * _angle_interval)
+		if diff < min_diff:
+			min_diff = diff
+			nearest_angle = i * _angle_interval
+	return nearest_angle
 
 func _highlight_slot(index: int):
 	_un_highlight_all()
